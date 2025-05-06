b874a2ba0d4c2fcb8de196dfcd37353463f1dd90
Glitch (glitch-hello-node) <none>
2025-05-06 19:44:22 +0000

    🌪✍️ Checkpoint ./AuthRoutes.js:59804604/92

--- AuthRoutes.js
+++ AuthRoutes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   checkUser,
   generateToken,
+  
+  addUser, // ✅ Add this
   getAllUsers,
   onBoardUser,
 } from "./AuthController.js";
@@ -9,6 +11,8 @@ import {
 const router = Router();
 
 router.post("/check-user", checkUser);
+
+router.post("/add-user", addUser); // ✅ Register the new route
 router.post("/onBoardUser", onBoardUser);
 router.get("/get-contacts", getAllUsers);
 router.get("/generate-token/:userId", generateToken);
