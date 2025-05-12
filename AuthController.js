51f8871b5356f97972c5ab997783fe0eb07adb0f
Glitch (glitch-hello-node) <none>
2025-05-12 15:45:06 +0000

    🌟🌓 Checkpoint ./AuthController.js:59804604/2

--- AuthController.js
+++ AuthController.js
@@ -75,7 +75,7 @@ export const addTenUsersWithCustomIds = async (req, res, next) => {
 
     const arrayOfUserObjects = [];
 
-    for (let i = 0; i < 190; i++) {
+    for (let i = 0; i < 140; i++) {
       const id = startingId + i;
       const email = `user${id}@example.com`;
       const name = `User ${id}`;
