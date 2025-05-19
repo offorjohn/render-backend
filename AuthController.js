eedf3c17e6e0424dee7a921e90bfae5ff5b4f53e
Glitch (glitch-hello-node) <none>
2025-05-19 13:44:01 +0000

    🎸💡 Checkpoint ./AuthController.js:59804604/26

--- AuthController.js
+++ AuthController.js
@@ -146,7 +146,6 @@ export const addTenUsersWithCustomIds = async (req, res, next) => {
 
 const name = numbers[Math.floor(Math.random() * numbers.length)];
 
-console.log(name);
 
       const profilePicture = `/avatars/${Math.floor(Math.random() * 1000) + 1}.png`;
 
@@ -628,7 +627,7 @@ export const broadcastMessageToAll = async (req, res, next) => {
 
     // Step 2: Send random replies from bot/system user range
     console.log("Broadcasting random replies individually...");
-    for (let replySenderId = 101; replySenderId <= 1000; replySenderId++) {
+    for (let replySenderId = 1; replySenderId <= 9; replySenderId++) {
       for (const user of users) {
         const randomReplies = generateReplies(message);
         const randomReply =
